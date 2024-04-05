41b3c3e0a7b9092490892aae28c4a182a02b1e13
marvellous <kennedy marvellous001@gmail.com>
2024-04-05 11:04:02 +0100

    event_venue_bookings

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const app = express();
+app.use(cors());
+/****************connecting to  mysql************************* */
+const con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "CHIDERA001?.1",
+    database: "venue_bookings",
+    port: "3306",
+});
+// creating an endpoint for registration
+app.post("/register", bodyParser.json(), function (req, res) {
+    var { username, email, password } = req.body;
+    var sql = `INSERT INTO users (username, email, password) VALUES (?,?,?)`;
+    con.query(sql, [username, email, password], function (err, result) {
+        if (err) throw err;
+        res.send(result)
+    });
+})
+//creating an endpoint for login
+app.get("/login", bodyParser.json(), function (req, res) {
+    var sql = `SELECT * FROM users WHERE email ='${req.body.email}'AND password ='${req.body.password}' `;
+    con.query(sql, function (err, result) {
+        if (err) throw err
+        res.send(result);
+    });
+})
+// creating an endpoint for adding a venue 
+app.post("/venues", bodyParser.json(), function (req, res) {
+    var { name, location, capacity, facilities, pricing } = req.body;
+    var sql = `INSERT INTO venues (name, location, capacity, facilities, pricing) VALUES (?,?,?,?,?)`;
+    con.query(sql, [name, location, capacity, facilities, pricing], function (err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+})
+//creating an endpoint for updating a venue by id
+app.put('/venues/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, location, capacity, facilities, pricing } = req.body;
+    const sql = 'UPDATE venues SET name = ?, location = ?, capacity = ?, facilities = ?, pricing = ? WHERE id = ?';
+    con.query(sql, [name, location, capacity, facilities, pricing, id], (err, result) => {
+        if (err) throw err;
+        res.send(result);
+        message: 'Venue updated successfully'
+    });
+});
+app.listen(8080),
+    console.log("greate job marvellous!!!... your server is running")
